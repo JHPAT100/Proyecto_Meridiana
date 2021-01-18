904a5e77cfdb582e97ba9d09073dac6818574415
sanho <sanho@DESKTOP-I58LJS4>
2021-01-17 20:25:06 -0500

    Commit inicial del proyecto

---
+++ src/proyecto_meridiana/Proyecto_Meridiana.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_meridiana;
+
+/**
+ *
+ * @author JHPAT<sanhool@live.com.mx>
+ */
+public class Proyecto_Meridiana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
