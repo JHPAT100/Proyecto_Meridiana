4993e34328912c00bea89668e3d421e618849de7
JHPAT <JHPAT>
2021-01-17 20:37:15 -0500

    Commit modificado con menu

--- src/proyecto_meridiana/Proyecto_Meridiana.java
+++ src/proyecto_meridiana/Proyecto_Meridiana.java
@@ -11,11 +11,28 @@ package proyecto_meridiana;
  */
 public class Proyecto_Meridiana {
 
+    public static void ingredientes_extra(){
+   
+        System.out.println("***Ingredientes Extra***");
+        System.out.println(" ");
+        System.out.println("Champiñones == 1");
+        System.out.println("Tocino ======= 2");
+        System.out.println("Chorizo ====== 3");
+        System.out.println("Piña ========= 4");
+        System.out.println("Aceitunas ==== 5");
+        System.out.println(" ");
+      
+        
+        
+}
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ingredientes_extra();
     }
     
 }
